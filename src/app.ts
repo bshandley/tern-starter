b670b50a75d6995ba6bb0ad0c667c3353ab0eb3a
Bradley S Handley <b@handley.io>
2022-02-22 02:41:29 -0600

    test commit for CI/CD

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.get('/ApiTest', (req: Request, res: Response) => {
-    res.send('The node server says Hello World!!');
+    res.send('The node server says Hello World!!!');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
